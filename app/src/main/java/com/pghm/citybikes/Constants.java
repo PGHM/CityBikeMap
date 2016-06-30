ba31d608318693c01527d92ee256f6ee81cb8a23
Jussi Päivinen <jussi.paivinen@gmail.com>
2016-06-30 20:14:38 +0300

    Log name

--- app/src/main/java/com/pghm/citybikes/Constants.java
+++ app/src/main/java/com/pghm/citybikes/Constants.java
@@ -6,4 +6,5 @@ package com.pghm.citybikes;
 public class Constants {
 
     public static final String BIKE_DATA_URL = "https://api.digitransit.fi/routing/v1/routers/hsl/bike_rental";
+    public static final String LOG_NAME = "CityBikes";
 }
