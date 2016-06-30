40ea5205256e7ff4b9234eb1b9d862f1d3b7185d
Jussi Päivinen <jussi.paivinen@gmail.com>
2016-06-30 19:16:50 +0300

    Constants with initial bike data url

---
+++ app/src/main/java/com/pghm/citybikes/Constants.java
@@ -0,0 +1,9 @@
+package com.pghm.citybikes;
+
+/**
+ * Created by Jussi on 30.6.2016.
+ */
+public class Constants {
+
+    public static final String BIKE_DATA_URL = "https://api.digitransit.fi/routing/v1/routers/hsl/bike_rental";
+}
