e3f1905ea3ba9d6bd28bb9dc86dba9e7c12f803c
Jussi Päivinen <jussi.paivinen@gmail.com>
2016-07-01 00:04:02 +0300

    Host interface and fragment loaded method

---
+++ app/src/main/java/com/pghm/citybikes/Elements/BikeStationFragmentHost.java
@@ -0,0 +1,11 @@
+package com.pghm.citybikes.Elements;
+
+/**
+ * Created by Jussi on 30.6.2016.
+ */
+
+/* Interface for the activity hosting fragments to communicate between the activity and fragments */
+public interface BikeStationFragmentHost {
+    /* Fragments can let the activity know that they are ready by using this method */
+    void fragmentLoaded();
+}
--- app/src/main/java/com/pghm/citybikes/fragments/BikeStationListFragment.java
+++ app/src/main/java/com/pghm/citybikes/fragments/BikeStationListFragment.java
@@ -1,16 +1,20 @@
 package com.pghm.citybikes.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.pghm.citybikes.Elements.BikeStationFragmentHost;
 import com.pghm.citybikes.R;
 
 import butterknife.ButterKnife;
 
 public class BikeStationListFragment extends Fragment {
+
+    private BikeStationFragmentHost host;
 
     /* Required empty initializer */
     public BikeStationListFragment() {}
@@ -20,6 +24,17 @@ public class BikeStationListFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
         ButterKnife.bind(view);
+        host.fragmentLoaded();
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            host = (BikeStationFragmentHost)context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement BikeStationFragmentHost");
+        }
     }
 }
