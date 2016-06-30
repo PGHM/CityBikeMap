d35310ad53f6630b90869015b4f2ae7e84d888e7
Jussi Päivinen <jussi.paivinen@gmail.com>
2016-06-30 20:36:35 +0300

    Test for bike data fetch

--- app/src/androidTest/java/com/pghm/citybikes/ApplicationTest.java
+++ app/src/androidTest/java/com/pghm/citybikes/ApplicationTest.java
@@ -2,7 +2,6 @@ package com.pghm.citybikes;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
 
 import com.pghm.citybikes.network.Callback;
 import com.pghm.citybikes.network.Requests;
@@ -11,16 +10,29 @@ import org.json.JSONArray;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertNotNull;
+
 @RunWith(AndroidJUnit4.class)
 public class ApplicationTest {
 
     @Test
-    public void testBikeDataFetch() {
+    public void testBikeDataFetch() throws InterruptedException {
+
+        /* Because we are testing async operation we need to wait for the result so we use countdown
+           latch that waits with await() for certain time for countDown() to occur or times out */
+        final CountDownLatch signal = new CountDownLatch(1);
+
         Requests.fetchBikeData(InstrumentationRegistry.getContext(), new Callback<JSONArray>() {
             @Override
             public void callback(JSONArray result) {
-                Log.d(Constants.LOG_NAME, "Huoh");
+                assertNotNull(result);
+                signal.countDown();
             }
         });
+
+        signal.await(10, TimeUnit.SECONDS);
     }
 }
