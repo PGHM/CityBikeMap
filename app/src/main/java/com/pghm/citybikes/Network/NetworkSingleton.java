73e1f405a542e6fac00463fc3ba3e10defce2779
Jussi Päivinen <jussi.paivinen@gmail.com>
2016-06-30 19:16:08 +0300

    Networksingleton to get volley requestqueue

---
+++ app/src/main/java/com/pghm/citybikes/Network/NetworkSingleton.java
@@ -0,0 +1,30 @@
+package com.pghm.citybikes.Network;
+
+import android.content.Context;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Jussi on 30.6.2016.
+ */
+public class NetworkSingleton {
+
+    private static NetworkSingleton instance;
+    private RequestQueue requestQueue;
+
+    private NetworkSingleton(Context context){
+        requestQueue = Volley.newRequestQueue(context);
+    }
+
+    public static NetworkSingleton getInstance(Context context){
+        if(instance == null){
+            instance = new NetworkSingleton(context);
+        }
+        return instance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        return this.requestQueue;
+    }
+}
