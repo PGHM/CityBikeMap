a3a9eb8c84d927f79e885b9011bc1a061e095f5d
Jussi Päivinen <jussi.paivinen@gmail.com>
2016-06-30 20:07:21 +0300

    Rename unit tests

---
+++ app/src/test/java/com/pghm/citybikes/UnitTests.java
@@ -0,0 +1,12 @@
+package com.pghm.citybikes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UnitTests {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
