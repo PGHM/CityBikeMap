2686b5ea60fe7803a123e46a195ba49f27186b0e
Jussi Päivinen <jussi.paivinen@gmail.com>
2016-07-03 18:59:24 +0300

    Make callback also when failing

--- app/src/main/java/com/pghm/citybikes/Network/Requests.java
+++ app/src/main/java/com/pghm/citybikes/Network/Requests.java
@@ -17,23 +17,23 @@ import org.json.JSONObject;
  * Created by Jussi on 30.6.2016.
  */
 public class Requests {
-
     public static void fetchBikeData(Context context, final Callback<JSONArray> callback) {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,
                 Constants.BIKE_DATA_URL, null, new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d(Constants.LOG_NAME, response.toString());
                         try {
                             callback.callback(response.getJSONArray("stations"));
                         } catch (JSONException e) {
                             e.printStackTrace();
+                            callback.callback(null);
                         }
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.e(Constants.LOG_NAME, error.toString());
+                        callback.callback(null);
                     }
                 });
         NetworkSingleton.getInstance(context).getRequestQueue().add(request);
