e58fa9e477e536a25bca8721b7d4f3523ca913dd
Jussi Päivinen <jussi.paivinen@gmail.com>
2016-07-03 20:40:28 +0300

    Listview for listfragment and initialization with empty view

--- app/src/main/java/com/pghm/citybikes/fragments/BikeStationListFragment.java
+++ app/src/main/java/com/pghm/citybikes/fragments/BikeStationListFragment.java
@@ -6,18 +6,29 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.pghm.citybikes.Elements.BikeStationFragmentHost;
+import com.pghm.citybikes.Elements.BikeStationListAdapter;
 import com.pghm.citybikes.R;
 import com.pghm.citybikes.models.BikeStation;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class BikeStationListFragment extends Fragment {
 
+    @BindView(R.id.list_view) ListView list;
+    @BindView(R.id.empty_view) TextView emptyView;
+
     private BikeStationFragmentHost host;
+    private BikeStationListAdapter adapter;
+    private List<BikeStation> listData = new ArrayList<>();
 
     /* Required empty initializer */
     public BikeStationListFragment() {}
@@ -26,7 +37,13 @@ public class BikeStationListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list, container, false);
-        ButterKnife.bind(view);
+        ButterKnife.bind(this, view);
+
+        adapter = new BikeStationListAdapter(
+                getActivity(), R.layout.element_bike_station, listData, host);
+        list.setEmptyView(emptyView);
+        list.setAdapter(adapter);
+
         host.fragmentLoaded();
         return view;
     }
