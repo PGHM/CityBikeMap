3515e3fd0fcd4b5dbfdfe4d9ff70d63fea9950c1
Jussi <jussi.paivinen@gmail.com>
2025-03-24 12:55:26 +0200

    Beautify bike station GraphQl query string

--- app/src/main/java/com/pghm/citybikes/Constants.java
+++ app/src/main/java/com/pghm/citybikes/Constants.java
@@ -6,7 +6,6 @@ public class Constants {
 
     public static final String BIKE_DATA_URL =
             "https://api.digitransit.fi/routing/v2/hsl/gtfs/v1?digitransit-subscription-key=9c43c39a28234c15b6e297f8995522cc";
-    public static final String BIKE_DATA_QUERY = "{ \"query\": \"{ vehicleRentalStations { id name lat lon availableSpaces { byType { count vehicleType { formFactor } } } availableVehicles { byType { count vehicleType { formFactor } } } }}\" }";
     public static final String LOG_NAME = "CityBikes";
 
     public static final LatLng DEFAULT_POSITION = new LatLng(60.169787, 24.938606);
@@ -16,4 +15,33 @@ public class Constants {
     public static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
     public static final int BIKE_STATION_INITIALIZE_RETRY_DELAY = 5000; // 5 seconds, milliseconds
     public static final int BIKE_STATION_UPDATE_INTERVAL = 10000; // 10 seconds, milliseconds
+
+    public static final String BIKE_DATA_QUERY = """
+    {
+        "query": "{
+            vehicleRentalStations {
+                id
+                name
+                lat
+                lon
+                availableSpaces {
+                    byType {
+                        count
+                        vehicleType {
+                            formFactor
+                        }
+                    }
+                }
+                availableVehicles {
+                    byType {
+                        count
+                        vehicleType {
+                            formFactor
+                        }
+                    }
+                }
+            }
+        }"
+    }
+    """;
 }
